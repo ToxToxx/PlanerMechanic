fc053372696c2874efb30e906747c6aedf13db1b
Matvey <sinema3d00@gmail.com>
2024-07-10 19:35:53 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerInput/InputManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Класс отвечающий за ввод игроком, на основе Unity Input System
+/// </summary>
+
+[RequireComponent(typeof(PlayerInput))]
+public class InputManager : MonoBehaviour
+{
+    public static PlayerInput PlayerInput;
+
+    public static Vector2 Movement;
+    public static bool JumpWasPressed;
+    public static bool JumpIsHeld;
+    public static bool JumpWasReleased;
+    public static bool RunIsHeld;
+
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+
+
+    //
+    private void Awake()
+    {
+        PlayerInput = GetComponent<PlayerInput>();
+
+        _moveAction = PlayerInput.actions["Move"];
+        _jumpAction = PlayerInput.actions["Jump"];
+
+    }
+
+    //Считываем значения для движения и прыжка
+    private void Update()
+    {
+        Movement = _moveAction.ReadValue<Vector2>();
+
+        JumpWasPressed = _jumpAction.WasPressedThisFrame();
+        JumpIsHeld = _jumpAction.IsPressed();
+        JumpWasReleased = _jumpAction.WasReleasedThisFrame();
+    }
+}
